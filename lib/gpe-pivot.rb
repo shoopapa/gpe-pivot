6da535f73122ce88d9c9f5ccf7a82ceec8f98063
shoopapa <joedavis29@gmail.com>
2019-08-14 22:52:51 -0400

    prototyped time series data

--- lib/gpe-pivot.rb
+++ lib/gpe-pivot.rb
@@ -19,26 +19,53 @@ module GPEPivot
       response.headers['Access-Control-Allow-Origin'] = '*'
     end
     
-    get "/" do
+    get "/galileo" do
         erb :home
     end
+
+    get "/time" do
+        erb :time_pivot
+    end
     
-    get "/data" do
+    get "/galileo_data" do
         File.read("lib/public/gal.json")
     end
 
+    get "/time_data" do
+        json_gen
+    end
+
+
     def json_gen
         JSON.generate([
-            {  name: "rich", age: 49, sex: :male     },
-            {  name: "bob", age: 53, sex: :male     },
-            {  name: "sue", age: 51, sex: :female     },
-            {  name: "joe", age: 19, sex: :male     },
-            {  name: "ben", age: 17, sex: :male     },
-            {  name: "lizzi", age: 15, sex: :female     },
-            {  name: "harry", age: 14, sex: :male     },
-            {  name: "jessi", age: 11, sex: :male     },
-            {  name: "richie", age: 8, sex: :male     },
-            {  name: "jamie", age: 6, sex: :male     },
+            {  name: "joe",    weight: [
+                {time:  1, value: 170}, 
+                {time:  2, value: 169},
+                {time:  3, value: 169},
+                {time:  4, value: 168},
+                {time:  5, value: 167},
+                {time:  6, value: 166},
+                {time:  7, value: 165},
+                {time:  8, value: 166},
+                {time:  9, value: 167},
+                {time: 10, value: 166},
+                {time: 11, value: 167}
+            ],
+            age: 19  },
+            {  name: "ben",    weight: [
+                {time:  1, value: 130}, 
+                {time:  2, value: 139},
+                {time:  3, value: 139},
+                {time:  4, value: 138},
+                {time:  5, value: 137},
+                {time:  6, value: 136},
+                {time:  7, value: 135},
+                {time:  8, value: 136},
+                {time:  9, value: 137},
+                {time: 10, value: 136},
+                {time: 11, value: 137}
+            ],
+            age: 17  }
         ])
     end
 
